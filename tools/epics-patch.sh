b8aaff714e52bb3d1627e355f50c96b2c176fdcf
Claudio Bisegni <bisegni@users.noreply.github.com>
2023-03-28 19:28:04 -0700

    Compilation and Configuration
    - fix osx compilation
    - enable cxx17 on boost

--- tools/epics-patch.sh
+++ tools/epics-patch.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
+OS=$(uname)
 
-if [ "$(uname)" == "Darwin" ]
+if [[ ${OS} == "Darwin" ]]
 then
+    echo "OSX patch"
     sed -i '' "s/OPT_CFLAGS_YES += -g/OPT_CFLAGS_YES += -g -std=c11/" $1/epics/src/epics/configure/os/CONFIG.darwinCommon.darwinCommon
     sed -i '' "s/OPT_CXXFLAGS_YES += -g/OPT_CXXFLAGS_YES += -g -std=c++11/" $1/epics/src/epics/configure/os/CONFIG.darwinCommon.darwinCommon 
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]
-then
-  echo Linux don\'t need any patch
-elif [ -n "$COMSPEC" -a -x "$COMSPEC" ]
-then 
-  echo $0: this script does not support Windows
 fi
 
