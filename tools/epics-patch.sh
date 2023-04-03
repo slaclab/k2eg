926f35f16a388f445d5f5ea01e1533eed97c3fab
Claudio Bisegni <bisegni@slac.stanford.edu>
2023-04-03 23:51:22 +0000

    fix epics patch in case of linux and clang compiler

--- tools/epics-patch.sh
+++ tools/epics-patch.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 OS=$(uname)
-COMPILER=$2
-echo $COMPILER
+CXX_COMPILER=$2
+C_COMPILER=$3
+echo $CXX_COMPILER
+echo $C_COMPILER
 if [[ ${OS} == "Darwin" ]]
 then
     echo "OSX patch"
@@ -12,8 +14,8 @@ elif [[ ${OS} == "Linux" ]] && [[ $COMPILER == *clang* ]] then
     sed -i -e "s/OPT_CFLAGS_YES += -g/OPT_CFLAGS_YES += -g -std=c11/" $1/epics/src/epics/configure/os/CONFIG_SITE.Common.linuxCommon
     sed -i -e "s/OPT_CXXFLAGS_YES += -g/OPT_CXXFLAGS_YES += -g -std=c++11/" $1/epics/src/epics/configure/os/CONFIG_SITE.Common.linuxCommon
     sed -i -e "s/#GNU         = NO/GNU         = NO/" $1/epics/src/epics/configure/os/CONFIG_SITE.Common.linux-x86_64
-    sed -i -e "s/#CMPLR_CLASS = clang/CMPLR_CLASS = clang/" $1/epics/src/epics/configure/os/CONFIG_SITE.Common.linux-x86_64
-    sed -i -e "s/#CC          = clang/CC          = clang/" $1/epics/src/epics/configure/os/CONFIG_SITE.Common.linux-x86_64
-    sed -i -e "s/#CCC         = clang++/CCC         = clang++/" $1/epics/src/epics/configure/os/CONFIG_SITE.Common.linux-x86_64
+    sed -i -e "s/#CMPLR_CLASS = clang/CMPLR_CLASS = $C_COMPILER/" $1/epics/src/epics/configure/os/CONFIG_SITE.Common.linux-x86_64
+    sed -i -e "s/#CC          = clang/CC          = $C_COMPILER/" $1/epics/src/epics/configure/os/CONFIG_SITE.Common.linux-x86_64
+    sed -i -e "s/#CCC         = clang++/CCC         = $CXX_COMPILER/" $1/epics/src/epics/configure/os/CONFIG_SITE.Common.linux-x86_64
 fi
 
