b4e636b04bde8834d4ca6d44eff3973466bbf01a
Claudio Bisegni <bisegni@slac.stanford.edu>
2023-04-04 03:50:05 +0000

    fix for debian

--- tools/epics-patch.sh
+++ tools/epics-patch.sh
@@ -2,14 +2,15 @@
 OS=$(uname)
 CXX_COMPILER=$2
 C_COMPILER=$3
+echo $OS
 echo $CXX_COMPILER
 echo $C_COMPILER
-if [[ ${OS} == "Darwin" ]]
+if [[ ${OS} == "Darwin" ]];
 then
     echo "OSX patch"
     sed -i '' "s/OPT_CFLAGS_YES += -g/OPT_CFLAGS_YES += -g -std=c11/" $1/epics/src/epics/configure/os/CONFIG.darwinCommon.darwinCommon
     sed -i '' "s/OPT_CXXFLAGS_YES += -g/OPT_CXXFLAGS_YES += -g -std=c++11/" $1/epics/src/epics/configure/os/CONFIG.darwinCommon.darwinCommon 
-elif [[ ${OS} == "Linux" ]] && [[ $COMPILER == *clang* ]] then
+elif [[ ${OS} == "Linux" ]] && [[ $COMPILER == *clang* ]]; then
     echo "Patch for clang on linux"
     sed -i -e "s/OPT_CFLAGS_YES += -g/OPT_CFLAGS_YES += -g -std=c11/" $1/epics/src/epics/configure/os/CONFIG_SITE.Common.linuxCommon
     sed -i -e "s/OPT_CXXFLAGS_YES += -g/OPT_CXXFLAGS_YES += -g -std=c++11/" $1/epics/src/epics/configure/os/CONFIG_SITE.Common.linuxCommon
