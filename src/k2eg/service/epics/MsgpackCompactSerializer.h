50ae7db35fd47bfdf17854782d5aded03f00d82b
Claudio Bisegni <bisegni@slac.stanford.edu>
2023-04-15 04:17:21 +0000

    add base msgpack ser class

---
+++ src/k2eg/service/epics/MsgpackCompactSerializer.h
@@ -0,0 +1,34 @@
+#ifndef K2EG_SERVICE_EPICS_MSGPACKCOMPACTSERIALIZER_H_
+#define K2EG_SERVICE_EPICS_MSGPACKCOMPACTSERIALIZER_H_
+
+#include <k2eg/service/epics/Serialization.h>
+#include <msgpack.hpp>
+
+namespace k2eg::service::epics_impl {
+
+class MsgpackCompactSerializer : public Serializer {
+    void processScalar(const epics::pvData::PVScalar* scalar, msgpack::packer<msgpack::sbuffer>& packer);
+    void processScalarArray(const epics::pvData::PVScalarArray* scalarArray, msgpack::packer<msgpack::sbuffer>& packer);
+    void processStructure(const epics::pvData::PVStructure* scalarArray, msgpack::packer<msgpack::sbuffer>& packer);
+    void processStructureArray(epics::pvData::PVStructureArray::const_svector structure_array, msgpack::packer<msgpack::sbuffer>& packer);
+public:
+    MsgpackCompactSerializer() = default;
+    virtual ~MsgpackCompactSerializer() = default;
+    SerializedMessageShrdPtr serialize(const ChannelData& message);
+};
+DEFINE_PTR_TYPES(MsgpackCompactSerializer)
+// Serialization message for json encoding
+class MsgpackCompactMessage : public SerializedMessage {
+    friend class MsgPackSerializer;
+    msgpack::sbuffer buf;
+    epics::pvData::PVStructure::const_shared_pointer epics_pv_struct;
+public:
+    MsgpackCompactMessage(epics::pvData::PVStructure::const_shared_pointer epics_pv_struct);
+    MsgpackCompactMessage() = default;
+    ~MsgpackCompactMessage() = default;
+    const size_t size() const;
+    const char* data() const;
+};
+DEFINE_PTR_TYPES(MsgpackCompactMessage)
+}
+#endif  // K2EG_SERVICE_EPICS_MSGPACKCOMPACTSERIALIZER_H_
