3ba60c58ab406d097103160399bc54762fda365d
Claudio Bisegni <bisegni@slac.stanford.edu>
2023-04-19 20:25:16 +0000

    implements put command

--- src/k2eg/controller/node/worker/PutCommandWorker.cpp
+++ src/k2eg/controller/node/worker/PutCommandWorker.cpp
@@ -1,8 +1,9 @@
+#include <k2eg/common/utility.h>
 #include <k2eg/controller/node/worker/PutCommandWorker.h>
-
 #include <k2eg/service/ServiceResolver.h>
 
-#include <k2eg/common/utility.h>
+#include "k2eg/controller/command/cmd/PutCommand.h"
+#include "k2eg/service/epics/EpicsPutOperation.h"
 
 using namespace k2eg::controller::node::worker;
 using namespace k2eg::controller::command;
@@ -17,15 +18,35 @@ using namespace k2eg::common;
 
 #pragma region PutCommandWorker
 PutCommandWorker::PutCommandWorker(EpicsServiceManagerShrdPtr epics_service_manager)
-    : logger(ServiceResolver<ILogger>::resolve())
-    , epics_service_manager(epics_service_manager) {}
+    : processing_pool(std::make_shared<BS::thread_pool>()), logger(ServiceResolver<ILogger>::resolve()), epics_service_manager(epics_service_manager) {}
 
-void PutCommandWorker::processCommand(ConstCommandShrdPtr command) {
-    if(command->type != CommandType::get) return;
-    ConstGetCommandShrdPtr g_ptr = static_pointer_cast<const GetCommand>(command);
-    logger->logMessage(STRING_FORMAT("Perform put command for %1%", g_ptr->channel_name), LogLevel::DEBUG);
+void
+PutCommandWorker::processCommand(ConstCommandShrdPtr command) {
+  if (command->type != CommandType::get) return;
+  ConstPutCommandShrdPtr p_ptr = static_pointer_cast<const PutCommand>(command);
+  logger->logMessage(STRING_FORMAT("Perform put command for %1%", p_ptr->channel_name), LogLevel::DEBUG);
+  auto put_op = epics_service_manager->putChannelData(p_ptr->channel_name, "value", p_ptr->value);
+  processing_pool->push_task(
+      &PutCommandWorker::checkPutCompletion, this, MakePutOpInfoShrdPtr(p_ptr->channel_name, p_ptr->value, MakePutCommandShrdPtr(put_op)));
+}
 
-
-    return;
+void
+PutCommandWorker::checkPutCompletion(PutOpInfoShrdPtr put_info) {
+  if (!put_info->op->isDone()) {
+    // re-enque the op class
+    processing_pool->push_task(&PutCommandWorker::checkPutCompletion, this, put_info);
+  } else {
+    switch (put_info->op->getState().event) {
+      case pvac::PutEvent::Fail:
+        logger->logMessage(STRING_FORMAT("Failed put command for %1% and value %2%", put_info->channel_name % put_info->value), LogLevel::INFO);
+        break;
+      case pvac::PutEvent::Cancel:
+        logger->logMessage(STRING_FORMAT("Cancelled put command for %1% and value %2%", put_info->channel_name % put_info->value), LogLevel::INFO);
+        break;
+      case pvac::PutEvent::Success:
+        logger->logMessage(STRING_FORMAT("Success put command for %1% and value %2%", put_info->channel_name % put_info->value), LogLevel::INFO);
+        break;
+    }
+  }
 }
 #pragma endregion PutCommandWorker
--- src/k2eg/controller/node/worker/PutCommandWorker.h
+++ src/k2eg/controller/node/worker/PutCommandWorker.h
@@ -2,6 +2,9 @@
 #define K2EG_CONTROLLER_NODE_WORKER_PUTCOMMANDWORKER_H_
 
 #include <k2eg/common/types.h>
+#include <k2eg/common/BS_thread_pool.hpp>
+#include "k2eg/service/epics/EpicsPutOperation.h"
+
 #include <k2eg/controller/node/worker/CommandWorker.h>
 #include <k2eg/service/epics/EpicsServiceManager.h>
 #include <k2eg/service/log/ILogger.h>
@@ -10,10 +13,19 @@
 
 namespace k2eg::controller::node::worker {
 
+struct PutOpInfo {
+    const std::string channel_name;
+    const std::string value;
+    service::epics_impl::ConstPutOperationShrdPtr op;
+};
+DEFINE_PTR_TYPES(PutOpInfo)
+
 //Class that implements the put epics command
 class PutCommandWorker : public CommandWorker {
+    std::shared_ptr<BS::thread_pool> processing_pool;
     k2eg::service::log::ILoggerShrdPtr logger;
     k2eg::service::epics_impl::EpicsServiceManagerShrdPtr epics_service_manager;
+    void checkPutCompletion(PutOpInfoShrdPtr put_info);
 public:
     PutCommandWorker(k2eg::service::epics_impl::EpicsServiceManagerShrdPtr epics_service_manager);
     virtual ~PutCommandWorker() = default;
