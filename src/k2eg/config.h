ddd2a9ae5073b616087a9cd93bdf7da9dfb246c2
Claudio Bisegni <bisegni@users.noreply.github.com>
2024-04-19 13:43:01 -0700

    upgrade dependencies ad some minor check (#77)
    - upgrade dependency
    - minor fixes
    +semrel: patch

--- src/k2eg/config.h
+++ src/k2eg/config.h
@@ -1,5 +1,5 @@
 #define k2eg_BOOST_VERSION "1.77.0"
-#define k2eg_EPICS_VERSION "R7.0.7"
+#define k2eg_EPICS_VERSION "R7.0.8"
 #define k2eg_LIBLZ4_VERSION "v1.9.2"
 #define k2eg_SQLITE_VERSION "version-3.41.2"
 #define k2eg_SQLITEORM_VERSION "v1.8.2"
