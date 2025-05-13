64e5a2e52604cf2bbf862e754902c4c19a5e98b1
Claudio Bisegni <bisegni@slac.stanford.edu>
2025-05-13 00:20:34 +0000

    minor refactoring

--- test/controller/NodeControllerSnapshot.cpp
+++ test/controller/NodeControllerSnapshot.cpp
@@ -131,7 +131,7 @@ TEST(NodeControllerSnapshot, SnapshotCommandWithMonitorMsgPackSer)
     boost::json::object             reply_msg;
     std::unique_ptr<NodeController> node_controller;
     auto                            publisher = std::make_shared<DummyPublisher>(work_done);
-    node_controller = initBackend(ncs_tcp_port, publisher, true, true);
+    node_controller = initBackend(ncs_tcp_port, publisher, false, true);
 
     // add the number of reader from topic
     dynamic_cast<ControllerConsumerDummyPublisher*>(publisher.get())->setConsumerNumber(1);
@@ -164,11 +164,11 @@ TEST(NodeControllerSnapshot, SnapshotCommandWithMonitorMsgPackSer)
 TEST(NodeControllerSnapshot, RepeatingSnapshotStartStop)
 {
     typedef std::map<std::string, msgpack::object> Map;
-    boost::json::object             reply_msg;
-    std::unique_ptr<NodeController> node_controller;
-       
-    auto                            publisher = std::make_shared<TopicCountedTargetPublisher>();
-    node_controller = initBackend(ncs_tcp_port, publisher, true, true);
+    boost::json::object                            reply_msg;
+    std::unique_ptr<NodeController>                node_controller;
+
+    auto publisher = std::make_shared<TopicCountedTargetPublisher>();
+    node_controller = initBackend(ncs_tcp_port, publisher, false, true);
 
     // add the number of reader from topic
     dynamic_cast<ControllerConsumerDummyPublisher*>(publisher.get())->setConsumerNumber(1);
@@ -191,7 +191,8 @@ TEST(NodeControllerSnapshot, RepeatingSnapshotStartStop)
 
     })}););
 
-    // wait for activating 1 ack message on app topic and wait for first snapshot 4 (header + 2 data event + completaion) messages
+    // wait for activating 1 ack message on app topic and wait for first snapshot 4 (header + 2 data event +
+    // completaion) messages
     publisher->wait_for({{"snapshot_name", 4}, {"app_reply_topic", 1}}, std::chrono::milliseconds(60000));
 
     // we need to have publish some message
@@ -200,14 +201,14 @@ TEST(NodeControllerSnapshot, RepeatingSnapshotStartStop)
 
     // stop the snapshot
     EXPECT_NO_THROW(node_controller->submitCommand({std::make_shared<const RepeatingSnapshotStopCommand>(RepeatingSnapshotStopCommand{
-        CommandType::repeating_snapshot_stop,
-        SerializationType::Msgpack,
-        "app_reply_topic",
-        "rep-id",
+        CommandType::repeating_snapshot_stop, 
+        SerializationType::Msgpack, 
+        "app_reply_topic", 
+        "rep-id", 
         "snapshot_name"
     })}););
-
-     while (node_controller->getTaskRunning(CommandType::repeating_snapshot))
+    // wait for the stop message succeed
+    while (node_controller->getTaskRunning(CommandType::repeating_snapshot))
     {
         sleep(1);
     }
