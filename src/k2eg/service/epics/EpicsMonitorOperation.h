d733f137153cc1cc1ed0134c92fa5dbe068eea14
Claudio Bisegni <bisegni@users.noreply.github.com>
2025-06-03 13:04:50 -0700

    Feature/test fast ioc (#104)
    - add fast ioc
    - increase speed to consume all the epics data when it arrives
    +semver: patch

--- src/k2eg/service/epics/EpicsMonitorOperation.h
+++ src/k2eg/service/epics/EpicsMonitorOperation.h
@@ -1,6 +1,7 @@
 #ifndef K2EG_SERVICE_EPICS_EPICSMONITOROPERATION_H_
 #define K2EG_SERVICE_EPICS_EPICSMONITOROPERATION_H_
 
+#include <cstddef>
 #include <k2eg/common/types.h>
 
 #include <k2eg/service/epics/EpicsData.h>
@@ -8,8 +9,8 @@
 #include <k2eg/service/epics/PVStructureMerger.h>
 
 #include <pvData.h>
-#include <sys/types.h>
 #include <pva/client.h>
+#include <sys/types.h>
 
 #include <atomic>
 #include <mutex>
@@ -31,7 +32,7 @@ protected:
 
 public:
     virtual ~MonitorOperation() = default;
-    virtual void                 poll(uint element_to_fetch = 2) const = 0;
+    virtual size_t               poll(size_t element_to_fetch = 100) const = 0;
     virtual EventReceivedShrdPtr getEventData() const = 0;
     virtual bool                 hasData() const = 0;
     virtual bool                 hasEvents() const = 0;
@@ -54,13 +55,12 @@ class MonitorOperationImpl : public pvac::ClientChannel::MonitorCallback, public
     mutable GetOperationUPtr             get_op;
     mutable EventReceivedShrdPtr         received_event;
     mutable std::mutex                   ce_mtx;
-    mutable bool                         has_data;
 
 public:
     MonitorOperationImpl(std::shared_ptr<pvac::ClientChannel> channel, const std::string& pv_name, const std::string& field = "field()");
     virtual ~MonitorOperationImpl();
     virtual void         monitorEvent(const pvac::MonitorEvent& evt) OVERRIDE FINAL;
-    virtual void         poll(uint element_to_fetch = 2) const OVERRIDE FINAL;
+    virtual size_t       poll(size_t element_to_fetch = 100) const OVERRIDE FINAL;
     virtual void         connectEvent(const pvac::ConnectEvent& evt) OVERRIDE FINAL;
     EventReceivedShrdPtr getEventData() const OVERRIDE FINAL;
     bool                 hasData() const OVERRIDE FINAL;
@@ -84,7 +84,7 @@ class CombinedMonitorOperation : public MonitorOperation
 public:
     CombinedMonitorOperation(std::shared_ptr<pvac::ClientChannel> channel, const std::string& pv_name, const std::string& principal_request, const std::string& additional_request);
     virtual ~CombinedMonitorOperation() = default;
-    virtual void poll(uint element_to_fetch = 2) const OVERRIDE FINAL;
+    virtual size_t poll(size_t element_to_fetch = 100) const OVERRIDE FINAL;
     /*
     Return alway the princiapl latest data
     merged with at least the last received
