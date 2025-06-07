7a449be52d615033495392cf0cb2dc5d97cc5ff3
Claudio Bisegni <bisegni@slac.stanford.edu>
2025-06-07 00:19:11 +0000

    add memory snpa during kafka publisher test to check for memory leaks

--- test/pubsub/common.h
+++ test/pubsub/common.h
@@ -1,5 +1,9 @@
 #include <k2eg/common/uuid.h>
 #include <k2eg/service/pubsub/pubsub.h>
+#include <k2eg/common/ProcSystemMetrics.h>
+
+#include <iomanip>
+#include <iostream>
 
 class Message : public k2eg::service::pubsub::PublishMessage {
     const std::string request_type;
@@ -34,3 +38,45 @@ class Message : public k2eg::service::pubsub::PublishMessage {
       return request_type;
     }
   };
+
+  ;
+
+// Class that owns ProcSystemMetrics and provides refresh, printHeader, and printSample
+class SystemResourcePrinter {
+    k2eg::common::ProcSystemMetrics metrics;
+
+public:
+    void refresh() {
+        metrics.refresh();
+    }
+
+    static void printHeader() {
+        std::cout << std::left
+                  << std::setw(12) << "VmRSS[kB]"
+                  << std::setw(12) << "VmHWM[kB]"
+                  << std::setw(14) << "VmSize[kB]"
+                  << std::setw(14) << "VmData[kB]"
+                  << std::setw(14) << "VmSwap[kB]"
+                  << std::setw(10) << "Threads"
+                  << std::setw(12) << "OpenFDs"
+                  << std::setw(12) << "MaxFDs"
+                  << std::setw(12) << "Uptime[s]"
+                  << std::setw(18) << "Starttime[jiffies]"
+                  << std::endl;
+    }
+
+    void printSample() const {
+        std::cout << std::left
+                  << std::setw(12) << metrics.vm_rss
+                  << std::setw(12) << metrics.vm_hwm
+                  << std::setw(14) << metrics.vm_size
+                  << std::setw(14) << metrics.vm_data
+                  << std::setw(14) << metrics.vm_swap
+                  << std::setw(10) << metrics.threads
+                  << std::setw(12) << metrics.open_fds
+                  << std::setw(12) << metrics.max_fds
+                  << std::setw(12) << metrics.uptime_sec
+                  << std::setw(18) << metrics.starttime_jiffies
+                  << std::endl;
+    }
+};
