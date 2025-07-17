eff3654fd1ea3929be0e22d126245c156df33c97
Claudio Bisegni <bisegni@slac.stanford.edu>
2025-07-17 02:00:51 +0000

    fixes

--- src/k2eg/service/epics/PVStructureMerger.h
+++ src/k2eg/service/epics/PVStructureMerger.h
@@ -13,7 +13,6 @@ struct PVStructureToMerge
 {
     epics::pvData::PVStructure::const_shared_pointer structure;
     const std::vector<std::string>& field_names;
-
 };
 
 // Manage the merge of a structure and permit to copy value from two structure
@@ -21,13 +20,27 @@ struct PVStructureToMerge
 class PVStructureMerger
 {
     epics::pvData::FieldBuilderPtr root_builder;
+
+    // Helper methods for structure definition copying
     void copyStructure(epics::pvData::FieldBuilderPtr builder, const epics::pvData::PVStructure* struct_ptr);
-    void copyValue(epics::pvData::PVStructure* dest_structure, const epics::pvData::PVStructure* src_structure);
+    void copyStructureDefinition(epics::pvData::FieldBuilderPtr builder, const epics::pvData::Structure* structDef);
+    void copyUnionDefinition(epics::pvData::FieldBuilderPtr builder, const epics::pvData::Union* unionDef);
+    
+    // Helper methods for value copying
+    void copyStructureValue(epics::pvData::PVStructure* dest_structure, const epics::pvData::PVStructure* src_structure);
+    void copyFieldValue(epics::pvData::PVField* dest_field, const epics::pvData::PVField* src_field);
+    
+    // Builder helper
+    void addFieldToBuilder(epics::pvData::FieldBuilderPtr builder, const std::string& field_name, const epics::pvData::PVField* field);
 
 public:
     PVStructureMerger();
     ~PVStructureMerger() = default;
+    
+    // Main merge method that combines structures and copies values
     epics::pvData::PVStructure::const_shared_pointer mergeStructureAndValue(const std::vector<PVStructureToMerge>& struct_ptr_vec);
+    
+    // Legacy methods
     void appendFieldFromStruct(std::vector<epics::pvData::PVStructure::const_shared_pointer> struct_ptr_vec, bool reset = false);
     epics::pvData::PVStructure::const_shared_pointer copyValue(std::vector<epics::pvData::PVStructure::const_shared_pointer> struct_ptr_vec);
     epics::pvData::PVStructure::const_shared_pointer getStructure() const;
